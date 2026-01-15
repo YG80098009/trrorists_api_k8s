f7190c4ae1f9463aba671d7dd64cafdc34b5c9f6
Yehuda_Gabrieli <yehuda8009@gmail.com>
2026-01-15 14:48:10 +0200

    end

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 import pandas as pd
 import io
-from models import Threat, ResponseThreat
+from models import ResponseThreat
 from db import collection
 
 app = FastAPI()
-
 @app.post("/top_threats", response_model=ResponseThreat)
-def get_top_threats(file: UploadFile = File(...)):
+async def get_top_threats(file: UploadFile = File(...)):
+    if not file.filename.endswith('.csv'):
+        raise HTTPException(status_code=400, detail="CSV error")
     try:
-        contents = file.read()
+        contents = await file.read()
         df = pd.read_csv(io.BytesIO(contents))
-        
-        df_sorted = df.sort_values(by="danger_rate")
+        df_sorted = df.sort_values(by="danger_rate", ascending=False)
         top_5_df = df_sorted.head(5)
         
         top_threats_list = []
@@ -22,18 +22,14 @@ def get_top_threats(file: UploadFile = File(...)):
                 "location": row["location"],
                 "danger_rate": int(row["danger_rate"])
             }
-          
-            object_threat = Threat(**threat_data)
-            top_threats_list.append(object_threat.dict())
+            top_threats_list.append(threat_data)
 
         if top_threats_list:
             collection.insert_many(top_threats_list)
-
         return {
             "count": len(top_threats_list),
             "top": top_threats_list
         }
 
     except:
-        raise HTTPException(status_code=422, detail=str(Exception))
-
+        raise HTTPException(status_code=422, detail="test error")
