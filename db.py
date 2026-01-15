6c02ad8c3ccd83e52a6a32f4eee6bb08de38e4d0
Yehuda_Gabrieli <yehuda8009@gmail.com>
2026-01-15 10:30:15 +0200

    adding docker ignore and models

---
+++ db.py
@@ -0,0 +1,16 @@
+import os
+from pymongo import MongoClient
+
+MONGO_HOST = os.getenv("MONGO_HOST", "mongo-0.mongo")
+MONGO_PORT = os.getenv("MONGO_PORT", "27017")
+MONGO_USERNAME = os.getenv("MONGO_USERNAME", "admin")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "secretpass")
+MONGO_DB = os.getenv("MONGO_DB", "threat_db")
+MONGO_AUTH_SOURCE = os.getenv("MONGO_AUTH_SOURCE", "admin")
+
+
+connection_string = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/?authSource={MONGO_AUTH_SOURCE}"
+
+client = MongoClient(connection_string)
+db = client[MONGO_DB]
+collection = db["top_threats"] 
